80e624d087fc67297d1937e0ee2707ab739f2729
shubwub <shubwub@gmail.com>
2020-11-15 10:49:16 +0000

    replaces out-of-box CSS with styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { background } from "./style_variables";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <AppElement></AppElement>;
 }
+
+const AppElement = styled.div`
+	text-align: center;
+	background-color: ${background};
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
 
 export default App;
---
+++ src/style_variables.js
@@ -0,0 +1,2 @@
+export const background = "#282c34";
+export const link = "#61dafb";
